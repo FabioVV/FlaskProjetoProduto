ac234c1009a2d024586f0c899737d4924ac0b7a6
fabioVV <fabio-varela@protonmail.com>
2022-11-18 22:01:25 -0300

    Teste

--- produtos_controller.py
+++ produtos_controller.py
@@ -7,6 +7,7 @@ from werkzeug.wrappers.response import Response
 from produtos import *
 from usuarios import *
 
+#Instacia a classe que guarda os usuarios
 usuarios = user()
 
 app = Flask(__name__)
@@ -15,6 +16,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 
 
 ######
+#FUncção que checa qual usuario esta logado
 def quem_esta_logado() -> InformacoesUsuarioSemSenha or None:
     if "usuario_logado" not in session:
         return None
@@ -23,7 +25,7 @@ def quem_esta_logado() -> InformacoesUsuarioSemSenha or None:
     logar(logado)
     return logado
 
-
+#FUnção que limita as funcionalidades do site, dando acesso apenas para usuarios logados
 def apenas_usuarios_logados(wrapped: Callable[..., Any]) -> Callable[..., Any]:
     from functools import wraps
 
@@ -37,36 +39,35 @@ def apenas_usuarios_logados(wrapped: Callable[..., Any]) -> Callable[..., Any]:
 
 ######
 
-
+#Home page é o login, essa função rendereiza a view do login, para se logar e se cadastrar
 @app.route("/")
 @app.route("/login")
 def tela_login():
-    mensagem = request.args.get("mensagem", "")
-    erro = request.args.get("erro", "")
-    return render_template("login.html", mensagem=mensagem, erro=erro)
+    return render_template("login.html")
 
-
+#Aqui, é o os dados do formulario de login chegao, para se logar ou tratar erros
 @app.route("/login", methods=["POST"])
 def login():
     logar(None)
     login_usuario = request.form.get("login", "")
     senha_usuario = request.form.get("senha", "")
     try:
+
         logar(usuarios.validar_login(login_usuario, senha_usuario))
         flash("Logado com sucesso!", "info")
         return redirect("/produto")
+
     except SenhaIncorreta as erro:
+
         flash("Senha incorreta!", "error")
         return redirect("/login")
 
-
+#Essa função renderiza a tela de criação de novo usuario
 @app.route("/usuario/novo")
 def tela_novo_user():
-    mensagem = request.args.get("mensagem", "")
-    erro = request.args.get("erro", "")
-    return render_template("novo_user.html", mensagem=mensagem, erro=erro)
+    return render_template("novo_user.html")
 
-
+#Aqui é onde os dados do formulario de criação de usuario chegam, para criar o usuario ou tratar erros
 @app.route("/usuario/novo", methods=["POST"])
 def novo_user():
     logar(None)
@@ -84,7 +85,7 @@ def novo_user():
         flash("Usuário ja existe!", "info")
         return redirect("/usuario/novo")
 
-
+#FUnção untilizada para deslogar o usuario, excluindo sua sessão
 @app.route("/logout", methods=["POST"])
 @apenas_usuarios_logados
 def logout():
@@ -92,20 +93,21 @@ def logout():
     flash("Você saiu da sua conta com sucesso!", "info")
     return redirect("/login")
 
-
+#Login propriamente dito, faz o login do usuario, caso os dados do formulario batam com os dados do banco de dados
 def logar(usuario: InformacoesUsuarioSemSenha or None):
     if usuario is not None:
         session["usuario_logado"] = usuario.login
+
     elif "usuario_logado" in session:
         del session["usuario_logado"]
 
-
+#Renderiza a tela que mostra todos os produtos registrados, apenas para usuarios logados, e a funcionalidade de criar novos produtos
 @app.route("/produto")
 @apenas_usuarios_logados
 def produtos():
     return render_template('todos_produtos.html', prod=prods)
 
-
+#AQui os dados do formulario de criação do produto chegam, e criam o produto ou trata erros.
 @app.route("/produto/novo", methods=["POST"])
 @apenas_usuarios_logados
 def novo_produto():
@@ -119,20 +121,20 @@ def novo_produto():
     flash("Produto criado com sucesso.", "info")
     return redirect("/produto")
 
-
+#Essa função renderiza a tela com o formulario ara criação do produto
 @app.route("/produto/novo")
 @apenas_usuarios_logados
 def novo_produto_form():
-    vazio = {"nome": "", "preco": ""}
-    return render_template("registrar_produto.html", prod=vazio, id_produto=-1)
+    produto_vazio = {"nome": "", "preco": ""}
+    return render_template("registrar_produto.html", prod=produto_vazio, id_produto=-1)
 
-
+#ESsa funçÃp renderiza a tela de edição do produto selecionado
 @app.route("/produto/editar/<int:id_produto>")
 @apenas_usuarios_logados
 def editar_prod(id_produto):
     return render_template("registrar_produto.html", prod=prods[id_produto], id_produto=id_produto)
 
-
+#Essa função recebe as modificações do formulario de ediçãp e caso esteja sem erros, edita o produtos
 @app.route("/produto/editar/<int:id_produto>", methods=["POST"])
 @apenas_usuarios_logados
 def editar(id_produto):
@@ -143,7 +145,7 @@ def editar(id_produto):
     flash("Produto editado com sucesso.", "info")
     return redirect("/produto")
 
-
+#FUnção que exclui o produto selecionado
 @app.route("/produto/excluir/<int:id_produto>", methods=["POST"])
 @apenas_usuarios_logados
 def excluir_prod(id_produto):
@@ -151,6 +153,15 @@ def excluir_prod(id_produto):
     flash("Produto excluido com sucesso.", "info")
     return redirect("/produto")
 
+@app.route("/produto/<int:id>")
+def verprod(id):
+    for p in prods:
+        if p == id:
+            return render_template("ver.html", produto = prods[id])
+    return render_template("produto404.html"),404
+
+
+    
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5002, debug=True)
